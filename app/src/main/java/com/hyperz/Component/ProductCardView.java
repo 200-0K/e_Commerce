aefc2d2b782cac092a6ed65b6a6a1be2a16abcf1
Faisal S.A <Ksa45560@gmail.com>
2021-11-25 20:43:56 +0300

    moved product card contents into its own xml

--- app/src/main/java/com/hyperz/Component/ProductCardView.java
+++ app/src/main/java/com/hyperz/Component/ProductCardView.java
@@ -1,11 +1,11 @@
 package com.hyperz.Component;
 
 import android.content.Context;
-import android.text.TextUtils;
-import android.util.TypedValue;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,19 +13,17 @@ import androidx.cardview.widget.CardView;
 import androidx.gridlayout.widget.GridLayout;
 
 import com.hyperz.Entity.Product;
+import com.hyperz.R;
 import com.squareup.picasso.Picasso;
 
 public class ProductCardView extends CardView {
     private Product product;
 
-    private LinearLayout verticalContainer;
     private ImageView uiImageView;
     private TextView uiTitle;
     private TextView uiPrice;
 
-    private final int cardWidth = 400;
-    private final int imageHeight = 300;
-    private final int titleHeight = 30;
+    private final int nCardPerRow = 2;
 
     public ProductCardView(@NonNull Context context, Product product) {
         super(context);
@@ -33,36 +31,24 @@ public class ProductCardView extends CardView {
 
         GridLayout.LayoutParams cardParams = new GridLayout.LayoutParams();
         cardParams.setMargins(25, 25, 25, 25);
+        this.setLayoutParams(cardParams);
         this.setContentPadding(15, 15, 15, 15);
         this.setClickable(true);
         this.setForeground(context.obtainStyledAttributes(new int[]{android.R.attr.selectableItemBackground}).getDrawable(0)); // ripple effect on click
-        this.setLayoutParams(cardParams);
 
-        verticalContainer = new LinearLayout(context);
-        FrameLayout.LayoutParams verticalParams = new FrameLayout.LayoutParams(cardWidth, LayoutParams.WRAP_CONTENT);
-        verticalContainer.setLayoutParams(verticalParams);
-        verticalContainer.setOrientation(LinearLayout.VERTICAL);
+        int hrPadding = this.getContentPaddingLeft() + this.getContentPaddingRight();
+        int hrMargin = cardParams.leftMargin + cardParams.rightMargin;
+        int cardWidth = (Resources.getSystem().getDisplayMetrics().widthPixels/nCardPerRow) - (hrMargin + hrPadding);
 
-        uiImageView = new ImageView(context);
-        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, imageHeight);
-        imageParams.setMargins(0, 0, 0, 5);
-        uiImageView.setLayoutParams(imageParams);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View cardContent = inflater.inflate(R.layout.product_card, null);
+        FrameLayout.LayoutParams contentParams = new FrameLayout.LayoutParams(cardWidth, LayoutParams.WRAP_CONTENT);
+        cardContent.setLayoutParams(contentParams);
+        uiImageView = cardContent.findViewById(R.id.prodcut_card_image);
+        uiTitle = cardContent.findViewById(R.id.product_card_title);
+        uiPrice = cardContent.findViewById(R.id.product_card_price);
 
-        uiTitle = new TextView(context);
-        LinearLayout.LayoutParams titleParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, titleHeight);
-        titleParams.setMargins(0 , 10, 0, 10);
-        uiTitle.setAutoSizeTextTypeUniformWithConfiguration(10, 20, 1, TypedValue.COMPLEX_UNIT_SP);
-        uiTitle.setSingleLine(true);
-        uiTitle.setEllipsize(TextUtils.TruncateAt.END);
-        uiTitle.setLayoutParams(titleParams);
-
-        uiPrice = new TextView(context);
-
-        verticalContainer.addView(uiImageView);
-        verticalContainer.addView(uiTitle);
-        verticalContainer.addView(uiPrice);
-
-        this.addView(verticalContainer);
+        addView(cardContent);
         updateCard(product);
     }
 
